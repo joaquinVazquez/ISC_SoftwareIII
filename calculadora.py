44a1f12f746dfed763781d1e4e07f9839f5d9bec
joaquinVazquez <joaquin.vazquez@unisa.edu.mx>
2026-05-09 12:57:03 -0600

    Ejercicios de la función print

---
+++ calculadora.py
@@ -0,0 +1,9 @@
+#Operaciones aritmecas
+print(2+2)
+print(100 * 12)
+print(1500 / 3)
+print(2 ** 10)
+
+#Cálculos adicionales
+print("Has vivido aproximadamente ",(36*365), "días")
+print("Precio con aumento: ", 350 * 1.15)
