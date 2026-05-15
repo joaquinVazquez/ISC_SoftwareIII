8b8e207b48b49338720a9aee396dbe50b0a9e004
joaquinVazquez <joaquin.vazquez@unisa.edu.mx>
2026-05-14 22:09:18 -0600

    Ejericio variables de negocio

---
+++ producto.py
@@ -0,0 +1,17 @@
+#Producto: Analizador de Performance para "Boxes" de CrossFit - 
+#¿Qué problema resuleve mi producto?
+#¿Quien lo usa y quien lo paga?
+#¿Cómo genera dinero?
+
+print("============================================================")
+print(f"Nombre del producto: CrossUp")
+print(f"Tipo de producto: App movil")
+print(f"Fundador: Joaquín Vázquez")
+print(f"Mercado objetivo: Atletas y entrenadore de crossfit")
+print(f"Modelo de negocio: Suscripción mensual")
+print("============================================================")
+
+print("¿Qué problema resuelve?: La falta de visualización de progreso real en atletas. No es una app de 'anotar pesos', sino un dashboard que integra datos de sensores o registros diarios para predecir picos de rendimiento y riesgo de lesiones.")
+print("Lo usan los coaches y atletas; lo paga el dueño del Box como un valor agregado a la membresía.")
+print("¿Cómo genera dinero?: B2B2C. Licencia mensual por Box (White label) para que el gimnasio ofrezca la 'tecnología de entrenamiento' a sus socios.")
+
