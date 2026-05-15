8ffc089c051c67b106143c4f8157be1c23b1d50a
joaquinVazquez <joaquin.vazquez@unisa.edu.mx>
2026-05-15 16:19:44 -0600

    Ejercicio for

---
+++ for.py
@@ -0,0 +1,5 @@
+funcionalidades = ["agregar_usuario", "asignar_rutina", "generar_reporte"]
+
+funcionalidades.append("login")
+for funcion in funcionalidades:
+    print(f"Funcionalidad del sistema: {funcion}")
